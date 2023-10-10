9a345752d83d216a432278e93ba3d4bd6149d48b
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-10-10 14:37:37 +0200

    header improvements

--- PHP_functions/header.php
+++ PHP_functions/header.php
@@ -1,28 +1,49 @@
 <?php
 function insertHeaderAndFooterStyle() {
-        echo '<style>
-    body {
-        margin: 0;
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-    }
+        echo "<style>
+        body {
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+        
+        .header {
+            background-color: #1E407A;
+            min-height: 18dvh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .about {
+            margin-right: 25vw;
+            color:#FECD3E;
+            
+            font-size: 5vh;
+            font-family: 'Nexa Bold', sans-serif;
     
-    .header {
-        background-color: #1E407A;
-        min-height: 18dvh;
-    }
-    
-    
-    .footer {
-        background-color: #FECD3E;
-        min-height: 11dvh;
-        margin-top: auto;
-    }
-</style>';
+        }
+        .logo-img {
+            min-height: 25px;
+            height: 70%;
+            max-height:100px;
+            min-width:100px;
+            width:15dvw;
+            max-width:400px;
+        }
+        .footer {
+            background-color: #FECD3E;
+            min-height: 11dvh;
+            margin-top: auto;
+        }
+    </style>";
     }
     function insertHeader() {
-        echo '<div class="header">header</div>';
+        echo '
+        <div class="header">
+            <div class="about">About us</div>
+            <div class="logo"> <img class="logo-img" src="../src/Logo/png/sunny_logos_slogan_yellow.png" alt="Sunny_logo"></div>    
+        </div>';
     }
 
 
