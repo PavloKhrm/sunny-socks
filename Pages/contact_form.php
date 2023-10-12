fcb831c7c15309482fefdc5ccd63acfb4678ff66
M1stake Sequence <m1stakesequence@m1stakes-air.wifi.nhlstenden.com>
2023-10-12 19:39:08 +0200

    Contact v2

--- Pages/contact_form.php
+++ Pages/contact_form.php
@@ -7,27 +7,28 @@
     <title>Контакты</title>
 </head>
 <body>
-<?php
-         require_once("../PHP_functions/header.php");
-    ?>
-   
+    <?php require_once("../PHP_functions/header.php"); ?>
+
+    <div class="container">
         <div class="contact-info">
             <h1>Need help?</h1>
-            <p>Gmail: <a href="mailto:info@yourdomain.com">sunny.socks@gmail.com</a></p>
-            <p>Phone: </p> 
-            <p>+31 672 391 1332</p>
+            <p class="email">Email:</p>
+            <p class="email2"> <a href="mailto:info@yourdomain.com">sunny.socks@gmail.com</a></p>
+            <p class="phone">Phone: </p>
+            <p class="phone2">+31 672 391 1332</p>
             <p>
-                <img src="../src/images_contact_form//facebook-logo.png" alt="Facebook" width="32" height="32">
+                <img src="../src/images_contact_page/facebook-logo.png" alt="Facebook" width="32" height="32">
                 <a href="https://www.facebook.com/yourpage">Sunny.Socks</a>
             </p>
             <p>
-                <img src="../src/images_contact_form/instagram-logo.png" alt="Instagram" width="32" height="32">
-                <a href="https://www.instagram.com/yourpage">Sunny.Socks</a>   
+                <img src="../src/images_contact_page/instagram-logo.png" alt="Instagram" width="32" height="32">
+                <a href="https://www.instagram.com/yourpage">Sunny.Socks</a>
             </p>
             <br>
-            <p class="contact-info-p">Just fill in a contact form 
-               and our customer service 
-               will contact you back
+            <p class="contact-info-p">
+                Just fill in a contact form
+                and our customer service
+                will contact you back
             </p>
         </div>
         <div class="contact-form">
@@ -39,14 +40,14 @@
                 <label for="email">Gmail:</label>
                 <input type="email" id="email" name="email" required>
 
-                <label for="message">Your question:</label>
-                <textarea id="message" name="message" rows="7" required></textarea>
+               <p><label for "message">Your question:</label>
+                <textarea id="message" name="message" rows="7" required></textarea></p> 
 
                 <button type="submit">Send</button>
             </form>
         </div>
-        <?php
-        require_once("../PHP_functions/footer.php");
-    ?>
+    </div>
+
+    <?php require_once("../PHP_functions/footer.php"); ?>
 </body>
 </html>
