4ba2dfdea0b3462fd4f59f850fc8d29f0d1a27e1
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-10-31 16:19:25 +0100

    completed functionality on shopping page (JS)

---
+++ JS_functions/shopping_page.js
@@ -0,0 +1,23 @@
+let currentColor = "red";
+let currentStyle = "uni";
+const changingImage = document.getElementById("mainImg");
+function changeImage() {
+    changingImage.style["opacity"] = 0;
+    
+    setTimeout(() => {
+        changingImage.src = "../src/sunny_socks_photos/packaging/png/catalogus_sokken_" + currentStyle+ "_" + currentColor + ".png" ;
+        changingImage.style["opacity"] = 1;//"Delayed for 1 second.";
+    }, "300");
+
+    
+}
+
+function changeColor(selectedColor) {
+    currentColor = selectedColor.toLowerCase();
+    changeImage();
+}
+console.log("changes color: " + currentColor);
+function changeStyle(selectedStyle) {
+    currentStyle = selectedStyle;
+    changeImage();
+}
