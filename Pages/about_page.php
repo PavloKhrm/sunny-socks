a9a2c0800bba6b2785b6a87dadb8220af26a0bc3
Nicole <nicole.gonzalez@student.nhlstenden.com>
2023-10-31 15:40:50 +0100

    First iteration of about page

---
+++ Pages/about_page.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <title>Document</title>
+    <link rel="stylesheet" href="./about_page_style.css" type="text/css">
+</head>
+<body>
+    <div class="header">
+        <h1>About Us</h1>
+    </div>
+
+    <div class="about_info">
+        <div class="branding">
+            <h2>Branding</h2>
+            <h3>Branding is the feeling someone gets when thinking of Sunny.
+                It goes beyond the product that is being sold. It is about
+                the values that a brand communicates.</h3>
+        </div>
+        <div class="branding_img"></div>
+        <div class="colorful">
+            <h2>Colorful</h2>
+            <h3>We like to add some color to one of the most everyday moments
+                of life: putting on your socks. We add literal color by designing
+                simple basic while socks with a pop of color.</h3>
+        </div>
+        <div class="colorful_img"></div>
+        <div class="brand_values">
+            <h2>Sunny's brand values:</h2>
+            <h3>Positivity is part of our DNA. Sunny likes to look at the
+                bright side. Every problem is an opportunity to come up with
+                a solution. These values are the core of Sunny's branding and
+                should be communicated.</h3>
+        </div>
+        <div class="brand_values_img"></div>
+        <div class="mission">
+            <h2>Mission</h2>
+            <h3>Sunny's mission is to brighten peoples day by providing high
+                quality, ethically produced socks.</h3>
+        </div>
+        <div class="mission_img"></div>
+        <div class="vision">
+            <h2>Vision</h2>
+            <h3>Sunny's Vision is to produce ethical and sustainable socks
+                that are available for everybody. To communicate all that
+                Sunny is, in a consistent way, a visual branding is developed</h3>
+        </div>
+        <div class="vision_img"></div>
+    </div>
+</body>
+</html>
