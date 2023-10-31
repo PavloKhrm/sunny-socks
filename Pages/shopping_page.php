09dee7b843d64ef2332e4bab078a7fa75a39fc56
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-10-31 14:47:08 +0100

    added functionality to the change color buttons

--- Pages/shopping_page.php
+++ Pages/shopping_page.php
@@ -1,4 +1,28 @@
 <?php require_once("../PHP_functions/footerAndHeader.php"); ?>
+<?php 
+
+
+    // if (!empty($_POST["colorSock"])) {
+    //     $currentColor = strtolower($_POST["colorSock"]); // gets a color from the user
+    // } //else $currentColor = "red"; // default color
+
+    // if (!empty($_POST["style"])) {
+    //     $currentStyle = $_POST["style"]; // gets a style from the user
+    // } //else $currentStyle = "uni"; // default style
+
+    $cookie_duration = 1000;
+    setcookie ("_color", $_POST['colorSock'], $cookie_duration);
+    setcookie ("_style", $_POST['style'], $cookie_duration);
+
+    $currentColor = isset($_COOKIE["_color"]) ? $_COOKIE["_color"] : "Red";
+    $currentStyle = isset($_COOKIE["_style"]) ? $_COOKIE["_style"] : "uni";
+
+    echo "current style: $currentStyle <br>";
+
+    echo "current color: $currentColor";
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,7 +39,7 @@
         <form action="./shopping_page.php" method="post">
             <div class="item column_left">
                 <h2>100% cotton</h2>
-                <img src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_red.png" alt="Sock"
+                <img src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_<?php echo $currentStyle?>_<?php echo strtolower($currentColor)?>.png" alt="Sock"
                     class="product">
                 <p>
                     <a href=""><img src="../" alt=""></a>
@@ -44,13 +68,13 @@
                 <h1>Choose the color</h1>
                 <div class="flex">
                     <?php
-                    $colors = ["Orange", "Pink", "Yellow", "Green", "Blue"];
+                    $colors = ["Red", "Pink", "Yellow", "Green", "Blue"];
                     foreach ($colors as $color) {
-                        echo '<input type="radio" name="color" id="color' . $color . '" value="' . $color . '" onclick="this.form.submit()">
+                        echo '<input type="radio" name="colorSock" id="color' . $color . '" value="' . $color . '" onclick="this.form.submit()">
                                 <label for="color' . $color . '">
                                     <img src="../src/sunny_illustrations/png/Sunny_socks_' . $color . '.png" alt="' . $color . ' Illustration" class="illustration">
                                     
-                                </label>';
+                                </label>' ;
                     }
                     ?>
                 </div>
