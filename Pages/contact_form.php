7c0cb8612e1343458fac22fbc16ac9fdbbfa4834
M1stake Sequence <m1stakesequence@M1stakes-MacBook-Air.local>
2023-11-01 23:02:29 +0100

    not done

--- Pages/contact_form.php
+++ Pages/contact_form.php
@@ -13,16 +13,16 @@
         <div class="contact-info">
             <h1>Need help?</h1>
             <p class="email">Email:</p>
-            <p class="email2"> <a href="mailto:info@yourdomain.com">sunny.socks@gmail.com</a></p>
+            <p class="email2"> <a href="mailto:info@yourdomain.com">Sunny.socks@gmail.com</a></p>
             <p class="phone">Phone: </p>
             <p class="phone2">+31 672 391 1332</p>
-            <p>
+            <p class="vertikal_el">
                 <img src="../src/images_contact_page/facebook-logo.png" alt="Facebook" width="32" height="32">
-                <a href="https://www.facebook.com/yourpage">Sunny.Socks</a>
+                <a href="https://www.facebook.com/yourpage">&nbsp;Sunny.Socks</a>
             </p>
-            <p>
+            <p class="vertikal_el">
                 <img src="../src/images_contact_page/instagram-logo.png" alt="Instagram" width="32" height="32">
-                <a href="https://www.instagram.com/yourpage">Sunny.Socks</a>
+                <a href="https://www.instagram.com/yourpage">&nbsp;Sunny.Socks</a>
             </p>
             <br>
             <p class="contact-info-p">
@@ -33,20 +33,32 @@
         </div>
         <div class="contact-form">
             <h2>Contact us</h2>
-            <form action="contact.php" method="post">
+            
                 <label for="name">Full name:</label>
                 <input type="text" id="name" name="name" required>
 
                 <label for="email">Gmail:</label>
                 <input type="email" id="email" name="email" required>
 
-               <p><label for "message">Your question:</label>
-                <textarea id="message" name="message" rows="7" required></textarea></p> 
+               <p>
+                <label for "message">Your question:</label>
+                <textarea id="message" name="message" rows="7" required></textarea>
+               </p> 
 
-                <button type="submit">Send</button>
-            </form>
+               <button class="buttton" id="openModalButton">Submit</button>
+               
+            
         </div>
     </div>
+
+    <div class="modal" id="modal">
+        <div class="modal-content">
+            <span class="close-modal-button" id="closeModalButton">&times;</span> 
+            <p class="modal-text"><br>Your message has been sent</p>
+            <img src="../src/images_contact_page/email.png" class="modal-image" alt="Изображение">
+        </div>
+    </div>
+     <script src="script.js"></script>
 
     <?php require_once("../PHP_functions/footer.php"); ?>
 </body>
