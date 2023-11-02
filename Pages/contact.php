86462a4c15e9770e6b6c3d91da93c1b90305903e
WoJulian <julian.wollesen@student.nhlstenden.com>
2023-11-02 11:00:26 +0100

    Revert "not done"
    This reverts commit 7c0cb8612e1343458fac22fbc16ac9fdbbfa4834.

--- Pages/contact.php
+++ Pages/contact.php
@@ -17,16 +17,16 @@ require_once("../PHP_functions/footerAndHeader.php"); ?>
         <div class="contact-info">
             <h1>Need help?</h1>
             <p class="email">Email:</p>
-            <p class="email2"> <a href="mailto:info@yourdomain.com">Sunny.socks@gmail.com</a></p>
+            <p class="email2"> <a href="mailto:info@yourdomain.com">sunny.socks@gmail.com</a></p>
             <p class="phone">Phone: </p>
             <p class="phone2">+31 672 391 1332</p>
-            <p class="vertikal_el">
+            <p>
                 <img src="../src/images_contact_page/facebook-logo.png" alt="Facebook" width="32" height="32">
-                <a href="https://www.facebook.com/yourpage">&nbsp;Sunny.Socks</a>
+                <a href="https://www.facebook.com/yourpage">Sunny.Socks</a>
             </p>
-            <p class="vertikal_el">
+            <p>
                 <img src="../src/images_contact_page/instagram-logo.png" alt="Instagram" width="32" height="32">
-                <a href="https://www.instagram.com/yourpage">&nbsp;Sunny.Socks</a>
+                <a href="https://www.instagram.com/yourpage">Sunny.Socks</a>
             </p>
             
             <p class="contact-info-p">
@@ -36,22 +36,19 @@ require_once("../PHP_functions/footerAndHeader.php"); ?>
             </p>
         </div>
         <div class="contact-form">
-            <h1>Contact us</h1>
-            
+            <h2>Contact us</h2>
+            <form action="contact.php" method="post">
                 <label for="name">Full name:</label>
                 <input type="text" id="name" name="name" required>
 
                 <label for="email">Gmail:</label>
                 <input type="email" id="email" name="email" required>
 
-               <p>
-                <label for "message">Your question:</label>
-                <textarea id="message" name="message" rows="7" required></textarea>
-               </p> 
+               <p><label for "message">Your question:</label>
+                <textarea id="message" name="message" rows="7" required></textarea></p> 
 
-               <button class="buttton" id="openModalButton">Submit</button>
-               
-            
+                <button type="submit">Send</button>
+            </form>
         </div>
     </div>
 
@@ -68,5 +65,6 @@ require_once("../PHP_functions/footerAndHeader.php"); ?>
     insertFooter();
     ?>
     
+    <?php require_once("../PHP_functions/footer.php"); ?>
 </body>
 </html>
