00c4c3d8c14b028edefd109c55cc245727a343c0
PavloKhrm <pavlo.kharaman@student.nhlstenden.com>
2023-11-02 18:30:50 +0100

    fix:email input check

--- JS_functions/script.js
+++ JS_functions/script.js
@@ -1,39 +1,49 @@
-// Находим элементы на странице
+
 const openModalButton = document.getElementById('openModalButton');
 const modal = document.getElementById('modal');
 const closeModalButton = document.getElementById('closeModalButton');
-const contactForm = document.getElementById('contact-form'); // Add this line
+const contactForm = document.getElementById('contact-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+const emailError = document.getElementById('emailError'); 
 
-// Закрыть модальное окно при загрузке страницы
 modal.style.display = 'none';
 
-// Открыть модальное окно
 openModalButton.addEventListener('click', () => {
-    modal.style.display = 'block';
+   if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || messageInput.value.trim() === '') {
+       alert('Please fill in all fields before submitting the form');
+   } else if (!emailPattern.test(emailInput.value)) {
+       alert('Please enter a valid email address');
+   } else {
+       modal.style.display = 'block';
+   }
 });
 
-// Закрыть модальное окно
+
 closeModalButton.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
-// Закрыть модальное окно при клике на фон
 window.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.style.display = 'none';
     }
 });
 
-// Добавьте обработчик события для отправки формы
+emailInput.addEventListener('input', () => {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(emailInput.value)) {
+        emailError.textContent = 'Please enter a valid email address';
+    } else {
+        emailError.textContent = '';
+    }
+});
+
 contactForm.addEventListener('submit', (e) => {
-    e.preventDefault(); // Prevent the default form submission
+    e.preventDefault(); 
 
-    // Place your form validation code here
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    const messageInput = document.getElementById('message');
-
-    // Add your form validation logic here
+   
     if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || messageInput.value.trim() === '') {
         alert('Please fill in all fields');
         return;
@@ -45,6 +55,14 @@ contactForm.addEventListener('submit', (e) => {
         return;
     }
 
-    // If all validation checks pass, you can submit the form here
-    // You may want to use AJAX to send the form data to your server for processing
+    modal.style.display = 'block';
+
+    nameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
 });
+
+
+
+
+
