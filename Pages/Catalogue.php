584461874e8894eb01437b6533da486656e95dce
ThirdPeter <peter.zlamala@student.nhlstenden.com>
2023-11-02 13:23:10 +0100

    edit:added right collumn and fixed style

--- Pages/Catalogue.php
+++ Pages/Catalogue.php
@@ -11,40 +11,42 @@
     <?php
     insertHeader("Catalogue"); //insert header from external file
     ?>
-
-    <div class="page_header">
-        <h class = "page_header_word header_word_blue" >Express</h1>
-        <h class = "page_header_word header_word_green" >yourself</h1>
-        <h class = "page_header_word header_word_orange" >with</h1>
-        <h class = "page_header_word header_word_pink" >our</h1>
-        <h class = "page_header_word header_word_yellow" >socks</h1>
-    </div>
-
-    <div class="page_content">
-        <div class= "left_collumn"> 
-            <div class="left_collumn_part"> <!--top part of left collumn-->
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Blue.png" alt="blue sock photo">
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Green.png" alt="blue sock photo">
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Pink.png" alt="blue sock photo">
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Red.png" alt="blue sock photo">
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Yellow.png" alt="blue sock photo">
-            </div>
-
-            <div class="left_collumn_part"> <!--bottom part of left collumn-->
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Blue.png" alt="blue sock photo">
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Green.png" alt="blue sock photo">
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Pink.png" alt="blue sock photo">
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Red.png" alt="blue sock photo">
-                <img class="image" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Yellow.png" alt="blue sock photo">
-            </div>
+    <div class="everything">
+        <div class="page_header">
+            <h class = "page_header_word header_word_blue" >Express</h1>
+            <h class = "page_header_word header_word_green" >yourself</h1>
+            <h class = "page_header_word header_word_orange" >with</h1>
+            <h class = "page_header_word header_word_pink" >our</h1>
+            <h class = "page_header_word header_word_yellow" >socks</h1>
         </div>
 
+        <div class="page_content">
+            <div class= "left_collumn"> 
+                <div class="left_collumn_part"> <!--top part of left collumn-->
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Blue.png" alt="blue striped sock photo">
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Green.png" alt="green striped ock photo">
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Pink.png" alt="pink striped sock photo">
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Red.png" alt="red striped sock photo">
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_stripes_Yellow.png" alt="yellow striped sock photo">
+                </div>
+
+                <div class="left_collumn_part"> <!--bottom part of left collumn-->
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Blue.png" alt="blue unicolor sock photo">
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Green.png" alt="green unicolor sock photo">
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Pink.png" alt="pink unicolor sock photo">
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Red.png" alt="red unicolor sock photo">
+                    <img class="image_left_collumn" src="../src/sunny_socks_photos/packaging/png/catalogus_sokken_uni_Yellow.png" alt="yellow unicolor sock photo">
+                </div>
+            </div>
 
 
 
-        <div class="right_collumn"> </div>   
-    </div>
 
+            <div class="right_collumn">
+                <img class="image_right_collumn" src="../src/images_catalogue/sneaker_with_yellow_sock.png" alt="">
+            </div>   
+        </div>
+        </div>
     <?php
     insertFooter(); //insert footer from external file
     ?>
