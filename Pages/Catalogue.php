89ac9fd2a30ee0ef12285dd71ee51f79a1159c08
ThirdPeter <peter.zlamala@student.nhlstenden.com>
2023-11-02 20:05:50 +0100

    edit: relocated link to css into header, Create: created backup.php and backup.css

--- Pages/Catalogue.php
+++ Pages/Catalogue.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/catalogue.css">
 </head>
 <body>
-    <link rel="stylesheet" href="./css/catalogue.css">
     <?php
     insertHeader("Catalogue"); //insert header from external file
     ?>
---
+++ Pages/backup.php
@@ -0,0 +1,16 @@
+<?php require_once("../PHP_functions/footerAndHeader.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/catalogue.css">
+</head>
+<body>
+    
+    <?php
+    insertFooter(); //insert footer from external file
+    ?>
+</body>
+</html>
