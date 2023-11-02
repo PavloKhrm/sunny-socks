d5de1f66b3e943ea14ea5baa14a8277901f6fb05
ThirdPeter <peter.zlamala@student.nhlstenden.com>
2023-11-02 12:18:35 +0100

    addeed catalogue.php and catalogue.css

---
+++ Pages/Catalogue.php
@@ -0,0 +1,32 @@
+<?php require_once("../PHP_functions/footerAndHeader.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <link rel="stylesheet" href="./css/catalogue.css">
+    <?php
+    insertHeader("Catalogue"); //insert header from external file
+    ?>
+
+    <div class="page_header">
+        <h class = "page_header_word header_word_blue" >Express</h1>
+        <h class = "page_header_word header_word_green" >yourself</h1>
+        <h class = "page_header_word header_word_orange" >with</h1>
+        <h class = "page_header_word header_word_pink" >our</h1>
+        <h class = "page_header_word header_word_yellow" >socks</h1>
+    </div>
+
+    <div class="page_content">
+
+    
+    </div>
+
+    <?php
+    insertFooter(); //insert footer from external file
+    ?>
+</body>
+</html>
